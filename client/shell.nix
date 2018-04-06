840fc259a17a545ec8df25c210214ec94be6ae97
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-06 13:49:50 +0900

    install cargo-web

--- client/shell.nix
+++ client/shell.nix
@@ -9,6 +9,10 @@ stdenv.mkDerivation {
     # rust environment
     (latest.rustChannels.nightly.rust.override { targets = [ "wasm32-unknown-unknown" ]; })
 
+    # openssl dev for cargo-web
+    openssl
+    pkgconfig
+
     # javascript envrionment
     nodePackages.npm
     nodejs
@@ -18,6 +22,7 @@ stdenv.mkDerivation {
     if ! type wasm-bindgen > /dev/null 2> /dev/null; then
       cargo install wasm-bindgen-cli --root "$PWD/.cargo"
     fi
+    cargo install cargo-web --root "$PWD/.cargo"
   '';
 }
 
