0f84adfbfc8567f7d483a0b44648537ec851c389
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-12 16:53:01 +0900

    rustfmt

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -2,10 +2,10 @@
 
 #[macro_use]
 extern crate stdweb;
-use stdweb::web::XmlHttpRequest;
-use stdweb::web::IEventTarget;
-use stdweb::web::event::*;
 use stdweb::js_export;
+use stdweb::web::event::*;
+use stdweb::web::IEventTarget;
+use stdweb::web::XmlHttpRequest;
 use stdweb::Promise;
 
 #[macro_use]
@@ -13,8 +13,8 @@ extern crate failure;
 use failure::Error;
 
 extern crate futures;
-use futures::{Future, Poll, Async};
 use futures::prelude::*;
+use futures::{Async, Future, Poll};
 
 extern crate serde;
 #[macro_use]
@@ -22,12 +22,12 @@ extern crate serde_derive;
 extern crate serde_json;
 
 extern crate ot;
-use ot::*;
-use ot::util::*;
 use ot::client::*;
+use ot::util::*;
+use ot::*;
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 struct Query {
@@ -50,9 +50,7 @@ struct JSError {
 
 impl JSError {
     fn new(s: String) -> Self {
-        JSError {
-            message: s,
-        }
+        JSError { message: s }
     }
 }
 
@@ -64,9 +62,7 @@ struct InvalidResponse {
 
 impl InvalidResponse {
     fn new(s: String) -> Self {
-        InvalidResponse {
-            message: s,
-        }
+        InvalidResponse { message: s }
     }
 }
 
@@ -121,16 +117,15 @@ impl<T, E> Future for Subject<T, E> {
         use SubjectInner::*;
 
         match self.0.try_borrow_mut() {
-            Ok(mut this) => 
-                match std::mem::replace(&mut *this, Done) {
-                    Pending => {
-                        *this = Pending;
-                        Ok(Async::NotReady)
-                    },
-                    Error(e) => Err(e),
-                    Ready(s) => Ok(Async::Ready(s)),
-                    Done => unreachable!(),
-                },
+            Ok(mut this) => match std::mem::replace(&mut *this, Done) {
+                Pending => {
+                    *this = Pending;
+                    Ok(Async::NotReady)
+                }
+                Error(e) => Err(e),
+                Ready(s) => Ok(Async::Ready(s)),
+                Done => unreachable!(),
+            },
             Err(_) => Ok(Async::NotReady),
         }
     }
@@ -143,13 +138,14 @@ impl AjaxConnection {
     fn retrieve_id(&self) -> Result<String, JSError> {
         match js!(retrieve_id()) {
             stdweb::Value::String(s) => Ok(s),
-            _ => Err(JSError::new("cannot retrieve id of this session".into()))
+            _ => Err(JSError::new("cannot retrieve id of this session".into())),
         }
     }
-
 }
 
-fn get<R: serde::de::DeserializeOwned + 'static>(path: &str) -> impl Future<Item = R, Error = Error> {
+fn get<R: serde::de::DeserializeOwned + 'static>(
+    path: &str,
+) -> impl Future<Item = R, Error = Error> {
     let xhr = Rc::new(XmlHttpRequest::new());
     let result: Subject<R, Error> = Subject::new();
 
@@ -158,12 +154,13 @@ fn get<R: serde::de::DeserializeOwned + 'static>(path: &str) -> impl Future<Item
         let mut result = result.clone();
         let _handle = xhr.add_event_listener::<ResourceLoadEvent, _>(move |_| {
             match xhr_.response_text() {
-                Ok(Some(s)) => 
-                    match serde_json::from_str::<R>(&s) {
-                        Ok(state) => result.ready(state),
-                        Err(e) => result.error(InvalidResponse::new(format!("cannot decode json: {}", e))),
-                    },
-                Ok(None) => result.error(InvalidResponse::new("response is empty".into())), 
+                Ok(Some(s)) => match serde_json::from_str::<R>(&s) {
+                    Ok(state) => result.ready(state),
+                    Err(e) => {
+                        result.error(InvalidResponse::new(format!("cannot decode json: {}", e)))
+                    }
+                },
+                Ok(None) => result.error(InvalidResponse::new("response is empty".into())),
                 Err(_) => result.error(JSError::new("XmlHttpRequest::response_text".into())),
             };
         });
@@ -171,15 +168,19 @@ fn get<R: serde::de::DeserializeOwned + 'static>(path: &str) -> impl Future<Item
 
     xhr.open("GET", path)
         .map_err(|_| JSError::new("XmlHttpRequest::open".into()))
-        .and_then(|_|
+        .and_then(|_| {
             xhr.send()
                 .map_err(|_| JSError::new("XmlHttpRequest::send_with_string".into()))
-        ).map_err(Into::into)
+        })
+        .map_err(Into::into)
         .into_future()
         .and_then(|_| result)
 }
 
-fn post<T: serde::Serialize + ?Sized, R: serde::de::DeserializeOwned + 'static>(path: &str, body: &T) -> impl Future<Item = R, Error = Error> {
+fn post<T: serde::Serialize + ?Sized, R: serde::de::DeserializeOwned + 'static>(
+    path: &str,
+    body: &T,
+) -> impl Future<Item = R, Error = Error> {
     let xhr = Rc::new(XmlHttpRequest::new());
     let result: Subject<R, Error> = Subject::new();
 
@@ -188,12 +189,13 @@ fn post<T: serde::Serialize + ?Sized, R: serde::de::DeserializeOwned + 'static>(
         let mut result = result.clone();
         let _handle = xhr.add_event_listener::<ResourceLoadEvent, _>(move |_| {
             match xhr_.response_text() {
-                Ok(Some(s)) => 
-                    match serde_json::from_str::<R>(&s) {
-                        Ok(state) => result.ready(state),
-                        Err(e) => result.error(InvalidResponse::new(format!("cannot decode json: {}", e))),
-                    },
-                Ok(None) => result.error(InvalidResponse::new("response is empty".into())), 
+                Ok(Some(s)) => match serde_json::from_str::<R>(&s) {
+                    Ok(state) => result.ready(state),
+                    Err(e) => {
+                        result.error(InvalidResponse::new(format!("cannot decode json: {}", e)))
+                    }
+                },
+                Ok(None) => result.error(InvalidResponse::new("response is empty".into())),
                 Err(_) => result.error(JSError::new("XmlHttpRequest::response_text".into())),
             };
         });
@@ -203,10 +205,10 @@ fn post<T: serde::Serialize + ?Sized, R: serde::de::DeserializeOwned + 'static>(
         .map_err(|_| JSError::new("XmlHttpRequest::open".into()))
         .map_err(Into::<Error>::into)
         .and_then(|_| serde_json::to_string(body).map_err(Into::into))
-        .and_then(|body|
+        .and_then(|body| {
             xhr.send_with_string(&body)
                 .map_err(|_| JSError::new("XmlHttpRequest::send_with_string".into()).into())
-        )
+        })
         .into_future()
         .and_then(|_| result)
 }
@@ -227,20 +229,24 @@ impl Connection for AjaxConnection {
     type StateFuture = Box<Future<Item = State, Error = Self::Error>>;
 
     fn get_latest_state(&self) -> Self::StateFuture {
-        Box::new(self.retrieve_id()
-            .map_err(Into::into)
-            .into_future()
-            .and_then(move |id| get(&format!("/realtime/{}", id)))
-            .map_err(Into::into))
+        Box::new(
+            self.retrieve_id()
+                .map_err(Into::into)
+                .into_future()
+                .and_then(move |id| get(&format!("/realtime/{}", id)))
+                .map_err(Into::into),
+        )
     }
 
     fn get_patch_since(&self, since_id: &Id) -> Self::Output {
         let since_id = since_id.0;
-        Box::new(self.retrieve_id()
-            .map_err(Into::into)
-            .into_future()
-            .and_then(move |id| get(&format!("/realtime/{}/patch?since_id={}", id, since_id)))
-            .map_err(Into::into))
+        Box::new(
+            self.retrieve_id()
+                .map_err(Into::into)
+                .into_future()
+                .and_then(move |id| get(&format!("/realtime/{}/patch?since_id={}", id, since_id)))
+                .map_err(Into::into),
+        )
     }
 
     fn send_operation(&self, base_id: Id, operation: Operation) -> Self::Output {
@@ -249,12 +255,14 @@ impl Connection for AjaxConnection {
             operation: operation,
         };
 
-        Box::new(self.retrieve_id()
-            .map_err(Into::<Error>::into)
-            .map_err(Into::into)
-            .into_future()
-            .and_then(move |id| post(&format!("/realtime/{}/patch", id), &patch))
-            .map_err(Into::into))
+        Box::new(
+            self.retrieve_id()
+                .map_err(Into::<Error>::into)
+                .map_err(Into::into)
+                .into_future()
+                .and_then(move |id| post(&format!("/realtime/{}/patch", id), &patch))
+                .map_err(Into::into),
+        )
     }
 }
 
@@ -340,52 +348,53 @@ fn get_connection() -> ConnectionHandle {
 #[js_export]
 fn get_client(_connection: ConnectionHandle) -> Promise {
     Promise::from_future({
-        CONNECTION.with(|connection| 
+        CONNECTION.with(|connection| {
             Client::with_connection(*connection)
                 .map(|client| {
-                    CLIENT.with(|client_box| {
-                        *client_box.borrow_mut() = Some(client)
-                    });
+                    CLIENT.with(|client_box| *client_box.borrow_mut() = Some(client));
                     ClientHandle
                 })
                 .map_err(|e| e.to_string())
-        )
+        })
     })
 }
 
 #[js_export]
 fn current_content(_client: ClientHandle) -> stdweb::Value {
-    CLIENT.with(|client| 
-        match client.borrow().as_ref().unwrap().current_content() {
+    CLIENT.with(
+        |client| match client.borrow().as_ref().unwrap().current_content() {
             Ok(c) => stdweb::Value::String(c),
             // TODO: log error or pass
             Err(_) => stdweb::Value::Null,
-        }
+        },
     )
 }
 
 #[js_export]
 fn push_operation(_client: ClientHandle, operation: OperationHandle) {
-    CLIENT.with(|client| 
+    CLIENT.with(|client| {
         OPERATIONS.with(|operations| {
             let operation = operations.borrow()[operation.0].clone();
-            client.borrow_mut().as_mut().unwrap().push_operation(operation);
+            client
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .push_operation(operation);
         })
-    )
+    })
 }
 
 #[js_export]
 fn send_to_server(_client: ClientHandle) -> Option<Promise> {
-    CLIENT.with(|client| 
-        match client.borrow_mut().as_mut().unwrap().send_to_server() {
-            Ok(f) => {
-                Some(Promise::from_future(f
-                    .map(|(id, operation)| Patch { id, operation })
-                    .map_err(|e| e.to_string())))
-            },
+    CLIENT.with(
+        |client| match client.borrow_mut().as_mut().unwrap().send_to_server() {
+            Ok(f) => Some(Promise::from_future(f.map(|(id, operation)| Patch {
+                id,
+                operation,
+            }).map_err(|e| e.to_string()))),
             // TODO: log error or pass
-            Err(_) => None 
-        }
+            Err(_) => None,
+        },
     )
 }
 
@@ -407,9 +416,12 @@ fn send_get_patch(_client: ClientHandle) -> Promise {
     CLIENT.with(|client| {
         let client = client.borrow();
         let client = client.as_ref().unwrap();
-        Promise::from_future(client.send_get_patch()
-            .map(|(id, operation)| Patch { id, operation })
-            .map_err(|e| e.to_string()))
+        Promise::from_future(
+            client
+                .send_get_patch()
+                .map(|(id, operation)| Patch { id, operation })
+                .map_err(|e| e.to_string()),
+        )
     })
 }
 
@@ -421,8 +433,7 @@ fn apply_patch(_client: ClientHandle, patch: Patch) -> stdweb::Value {
         let client = client.as_mut().unwrap();
         match client.apply_patch(patch.id, patch.operation) {
             Ok(()) => stdweb::Value::Null,
-            Err(e) => stdweb::Value::String(e.to_string())
+            Err(e) => stdweb::Value::String(e.to_string()),
         }
     })
 }
-
