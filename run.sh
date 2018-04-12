7715a2ea75f8b6d19f858b99e7aeebccd2ac3af6
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-12 22:26:51 +0900

    added helper script for building/running

--- run.sh
+++ run.sh
@@ -1,14 +1,2 @@
-#!/bin/sh
-
-if [ -z "$1" -o -z "$2" ]
-then
-  echo "not enough arguments"
-  exit 1
-fi
-
-CONTAINER=$(docker create pandaman64/satysfi-playground)
-docker cp "$1" "$CONTAINER:/tmp/input.saty" > /dev/null
-docker start -a "$CONTAINER"
-docker cp "$CONTAINER:/tmp/output.pdf" "$2" > /dev/null
-docker rm "$CONTAINER" > /dev/null
-
+(cd server && \
+    nix-shell --run "cargo run")
