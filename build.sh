a9ab8ed95381b84fdf9966e80a93a9f46e79739c
pandaman64 <kointosudesuyo@infoseek.jp>
2018-04-19 11:25:35 +0900

    serialize build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 (cd client && \
-    cargo web build --target=wasm32-unknown-unknown --release)
-cp client/target/wasm32-unknown-unknown/release/client.js client/target/wasm32-unknown-unknown/release/client.wasm server/assets
-sed -i 's/client.wasm/\/client.wasm/' server/assets/client.js
+    cargo web build --target=wasm32-unknown-unknown --release) && \
+cp client/target/wasm32-unknown-unknown/release/client.js client/target/wasm32-unknown-unknown/release/client.wasm server/assets && \
+sed -i 's/client.wasm/\/client.wasm/' server/assets/client.js && \
 (cd server && \
     cargo build)
