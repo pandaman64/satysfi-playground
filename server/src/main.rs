befa3d11737d9187752caf9cd1912975152171f0
pandaman64 <kointosudesuyo@infoseek.jp>
2018-06-09 20:32:28 +0900

    actix

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,18 @@
-#![feature(plugin, custom_derive)]
-#![plugin(rocket_codegen)]
-
 #[macro_use]
 extern crate serde_derive;
 
 #[macro_use]
 extern crate lazy_static;
 
-extern crate rocket;
-use rocket::response::NamedFile;
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest, HttpResponse, fs::NamedFile, http, Json, ResponseError, middleware::Logger, fs::StaticFiles};
 use std::path::PathBuf;
 
-extern crate rocket_contrib;
-use rocket_contrib::Json;
-use rocket_contrib::Template;
+#[macro_use]
+extern crate tera;
+use tera::Tera;
+
+extern crate env_logger;
 
 use std::fs::File;
 use std::io::Read;
@@ -22,11 +21,33 @@ extern crate sha2;
 
 #[macro_use]
 extern crate failure;
-use failure::Error;
 
-mod realtime;
+//mod realtime;
 mod util;
 use util::*;
+
+lazy_static! {
+    static ref TEMPLATE: Tera = compile_templates!("templates/*.html");
+}
+
+#[derive(Debug, Fail)]
+enum Error {
+    #[fail(display = "Template Error: {}", _0)]
+    TemplateError(String),
+    #[fail(display = "IO Error: {}", _0)]
+    IOError(std::io::Error),
+    #[fail(display = "Compile Error")]
+    CompileError,
+    #[fail(display = "Uri Error: {}", _0)]
+    UriSegmentError(actix_web::error::UriSegmentError),
+}
+
+impl ResponseError for Error {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::InternalServerError()
+            .finish()
+    }
+}
 
 const DEFAULT_CODE: &'static str = "@require: stdjabook
 
@@ -42,34 +63,31 @@ document (|
 const DEFAULT_PDF: &'static str =
     "9165b5e8141ca2457c13bf72fbf07f01e795ac5e3bb112f5ed01bc08fb9cbe1a";
 
-#[get("/permalink/<query>")]
-fn permalink(query: String) -> Template {
-    Template::render(
-        "index",
-        &create_context(query, DEFAULT_CODE.into(), DEFAULT_PDF.into()),
-    )
+fn permalink(query: String) -> Result<HttpResponse, Error> {
+    let s = TEMPLATE
+        .render(
+            "index.html",
+            &create_context(query, DEFAULT_CODE.into(), DEFAULT_PDF.into()),
+        )
+        .map_err(|e| Error::TemplateError(e.description().to_owned()))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-#[get("/")]
-fn index() -> Template {
-    Template::render(
-        "index",
-        &create_context(
-            "9165b5e8141ca2457c13bf72fbf07f01e795ac5e3bb112f5ed01bc08fb9cbe1a".to_string(),
-            DEFAULT_CODE.into(),
-            DEFAULT_PDF.into(),
-        ),
-    )
+fn index(_: HttpRequest) -> Result<HttpResponse, Error> {
+    let s = TEMPLATE 
+        .render(
+            "index.html",
+            &create_context(
+                "9165b5e8141ca2457c13bf72fbf07f01e795ac5e3bb112f5ed01bc08fb9cbe1a".to_string(),
+                DEFAULT_CODE.into(),
+                DEFAULT_PDF.into(),
+            ),
+        )
+        .map_err(|e| Error::TemplateError(e.description().to_owned()))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(s))
 }
 
-// for non-pdf files
-#[get("/<path..>", rank = 2)]
-fn assets(path: PathBuf) -> Option<NamedFile> {
-    let path = PathBuf::new().join("assets").join(path);
-    NamedFile::open(path).ok()
-}
-
-use rocket::response;
+/*
 // https://github.com/SergioBenitez/Rocket/issues/95#issuecomment-354824883
 struct CachedFile(NamedFile);
 
@@ -80,46 +98,46 @@ impl<'a> response::Responder<'a> for CachedFile {
             .ok()
     }
 }
+*/
 
-#[get("/files/<hash..>")]
-fn files(hash: PathBuf) -> Option<CachedFile> {
+fn files(req: HttpRequest) -> Result<NamedFile, Error> {
+    let hash: PathBuf = req.match_info()
+        .query("hash")
+        .map_err(Error::UriSegmentError)?;
     match NamedFile::open(make_output_path(&hash)) {
-        Ok(file) => Some(file),
+        Ok(file) => Ok(file),
         _ => File::open(make_input_path(&hash))
-            .ok()
-            .and_then(|mut f| {
-                let mut content = String::new();
-                f.read_to_string(&mut content).ok()?;
-                compile(content).ok()
-            })
-            .and_then(|output| NamedFile::open(output.name).ok()),
-    }.map(CachedFile)
+                .map_err(Error::IOError)
+                .and_then(|mut f| {
+                    let mut content = String::new();
+                    f.read_to_string(&mut content)
+                        .map_err(Error::IOError)?;
+                    compile(content)
+                        .map_err(|_| Error::CompileError)
+                })
+                .and_then(|output| NamedFile::open(output.name).map_err(Error::IOError)),
+    }
 }
 
-#[post("/compile", format = "application/json", data = "<input>")]
 fn compile_handler(input: Json<Input>) -> Result<Json<Output>, Error> {
-    compile(input.content.to_string()).map(Json)
+    compile(input.content.to_string())
+        .map(Json)
+        .map_err(|_| Error::CompileError)
 }
 
 fn main() {
-    rocket::ignite()
-        .mount(
-            "/",
-            routes![
-                // basic functionalities
-                index,
-                assets,
-                permalink,
-                files,
-                compile_handler,
-                // for realtime editing
-                realtime::get_session,
-                realtime::get_latest,
-                realtime::get_patch,
-                realtime::patch_session,
-                realtime::new_session,
-            ],
-        )
-        .attach(Template::fairing())
-        .launch();
+    env_logger::init();
+
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.method(http::Method::GET).with(index))
+            .handler("/assets", StaticFiles::new("./assets"))
+            .resource("/files/{hash}", |r| r.method(http::Method::GET).with(files))
+            .resource("/compile", |r| r.method(http::Method::POST).with(compile_handler))
+            .resource("/permalink/{query}", |r| r.method(http::Method::GET).with(permalink)) 
+            .middleware(Logger::default())
+    })
+    .bind("0.0.0.0:8000")
+    .unwrap()
+    .run();
 }
