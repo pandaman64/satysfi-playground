9aff460e641d774842919f7d882d97bc7a8fa8df
pandaman64 <kointosudesuyo@infoseek.jp>
2021-10-23 21:25:08 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,8 @@
+{
+  description = "SATySFi Playground";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  };
+}
